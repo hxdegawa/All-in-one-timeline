fc3777b51c0db54b7e01ccd62276f055c9ce56b0
Motone Adachi <motone_16n1100030@nnn.ed.jp>
2017-12-09 15:35:40 +0900

    *_request.js was integrated. Set DB.

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const express = require('express');
-const router = express.Router();
+const router  = express.Router();
+const db      = new require('sqlite3').verbose().Database('db.sqlite3');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
